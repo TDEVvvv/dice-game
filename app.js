5e7d230493e34caaaf599b1f3726514293338580
temuge <e.temuge@yahoo.com>
2022-06-10 21:17:45 +0800

    project iin ehnii huvisagchidiig uusgesen

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var scores = [0, 0]
 // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0
 // шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = 2;
+var dice = Math.floor(Math.random() * 6) + 1;
 
-console.log('шоо : '+dice)
+
+
+// prepare to start game
+document.querySelector('#score-0').textContent = 0;
+document.querySelector('#score-1').textContent = 0;
+document.querySelector('#current-0').textContent = 0;
+document.querySelector('#current-1').textContent = 0;
+
+document.querySelector('.dice').style.display = 'none';
