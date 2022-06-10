6428eda381dc13dd2d89e76a70f71db0a030b4a5
temuge <e.temuge@yahoo.com>
2022-06-10 23:24:49 +0800

    togloomoo hiij duuslaa

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var isNewGame;
 var activePlayer = 0;
 var scores = [0, 0]
 var roundScore = 0;
@@ -5,7 +6,7 @@ var diceDom = document.querySelector('.dice');
 initGame()
 function initGame(){
     // тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-
+isNewGame = false;
 activePlayer = 0;
 // тоглогчдийн цуглуулсан оноо хадгалах хувьсагч
 scores = [0, 0]
@@ -29,6 +30,9 @@ document.getElementById('name-1').textContent = 'Player 2';
 }
 // shoog shideh event listiner
 document.querySelector('.btn-roll').addEventListener('click',function(){
+    if(isNewGame !==true){
+
+    
     // 1-6 hoorond sanamsargui too
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     // shoonii zurgiin gargaj irne
@@ -47,20 +51,28 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         
     }
     
-});
+}else {
+    alert('game is over')
+}});
 // hold button event listener
 document.querySelector('.btn-hold').addEventListener('click', function(){
+    if(isNewGame!==true){
+
+    
     // ug toglogchiin eeljnii onoog global onoo deer nemj ugnu
     scores[activePlayer] = scores[activePlayer]+roundScore;
     document.getElementById('score-'+activePlayer).textContent=scores[activePlayer]
-    if(scores[activePlayer]>=100){
+    if(scores[activePlayer]>=10){
         document.getElementById('name-'+activePlayer).textContent='Winner!!!'
+        isNewGame = true;
         
     }else {
         switchToNextPlayer()
     }
     
-});
+}else {
+    alert('game is over')
+}});
 function switchToNextPlayer(){
     roundScore = 0;
     document.getElementById('current-'+activePlayer).textContent=0
