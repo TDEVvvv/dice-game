9df285f68f6d78cc47d7e6de338070ba0b0b763d
temuge <e.temuge@yahoo.com>
2022-06-10 22:17:54 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // тоглогчдийн цуглуулсан оноо хадгалах хувьсагч
 var scores = [0, 0]
@@ -17,10 +17,34 @@ document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
 var diceDom = document.querySelector('.dice');
-
 diceDom.style.display = 'none';
+// shoog shideh event listiner
 document.querySelector('.btn-roll').addEventListener('click',function(){
+    // 1-6 hoorond sanamsargui too
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // shoonii zurgiin gargaj irne
     diceDom.style.display = 'block';
+    // buusan sanamsargui toog hargalzah zurgiig gargana
     diceDom.src ='dice-'+diceNumber+'.png'
+    // buusan too 1 ees yalgaatai bol idevhtei toglogchiin onoog nemegduulne
+    if(diceNumber!==1){
+        // 1 ees yalgaatai too buulaa. Buusan toog toglogchid nemj ugnu
+        roundScore = roundScore+ diceNumber
+        document.getElementById('current-'+activePlayer).textContent=roundScore
+    }else {
+        diceDom.style.display = 'none';
+        roundScore=0;
+        document.getElementById('current-'+activePlayer).textContent = 0;
+        // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0)
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        // if(activePlayer===0){
+        //     activePlayer = 1;
+        // }else {
+        //     activePlayer = 0
+        // }
+    }
+    
 });
